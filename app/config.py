245ca9ae321bb30210e321cad0a24aae655b62e3
mrnninster <adefolahanakinsola@gmail.com>
2023-01-28 13:34:57 +0100

    recaptcha for prod

--- app/config.py
+++ app/config.py
@@ -8,11 +8,17 @@ class BaseConfig:
     is_production = os.environ.get("IS_PRODUCTION")
     if is_production == True:
         ENV = "production"
+        TESTING = False
+        RECAPTCHA_PUBLIC_KEY = os.environ.get('PROD_CAPTCHA_PUBLIC')
+        RECAPTCHA_PRIVATE_KEY = os.environ.get('PROD_CAPTCHA_PRIVATE')
     else:
         ENV = "development"
+        TESTING = True
+        RECAPTCHA_PUBLIC_KEY = os.environ.get('DEV_CAPTCHA_PUBLIC')
+        RECAPTCHA_PRIVATE_KEY = os.environ.get('DEV_CAPTCHA_PRIVATE')
+
+    print(RECAPTCHA_PRIVATE_KEY,RECAPTCHA_PUBLIC_KEY)
+        
     SQLALCHEMY_POOL_RECYCLE = 299
-    TESTING = os.environ.get("CAPTCHA_TESTING_STATUS")
-    RECAPTCHA_PUBLIC_KEY = os.environ.get('CAPTCHA_PUBLIC')
-    RECAPTCHA_PRIVATE_KEY = os.environ.get('CAPTCHA_PRIVATE')
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
