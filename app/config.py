575560a12cfe44f42f62caa8ca140a8ff4777eaa
mrnninster <adefolahanakinsola@gmail.com>
2023-01-28 13:58:07 +0100

    fixing config

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 
 class BaseConfig:
     is_production = os.environ.get("IS_PRODUCTION")
-    if is_production == True:
+    if is_production == 'True':
         ENV = "production"
         TESTING = False
         RECAPTCHA_PUBLIC_KEY = os.environ.get('PROD_CAPTCHA_PUBLIC')
@@ -16,8 +16,6 @@ class BaseConfig:
         TESTING = True
         RECAPTCHA_PUBLIC_KEY = os.environ.get('DEV_CAPTCHA_PUBLIC')
         RECAPTCHA_PRIVATE_KEY = os.environ.get('DEV_CAPTCHA_PRIVATE')
-
-    print(RECAPTCHA_PRIVATE_KEY,RECAPTCHA_PUBLIC_KEY)
         
     SQLALCHEMY_POOL_RECYCLE = 299
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
